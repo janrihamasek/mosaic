2c7b73a378acdc60a583c66611f83e9a8224bc84
janrihamasek <janrihamasek@gmail.com>
2025-11-12 13:30:41 +0100

    mobile_bootstrap_capacitor_project

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mosaic.app',
+  appName: 'Mosaic',
+  webDir: '../frontend/build',
+  bundledWebRuntime: false
+};
+
+export default config;
